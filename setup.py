6acb5675215a2250987b57327d4a7bf7843d331d
Shane Lee <ksieee@gmail.com>
2017-04-07 17:36:14 +0800

    init

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ for package in find_packages():
         sys.path.insert(0, os.path.join(os.path.dirname(__file__), package))
 from package_info import USERNAME, VERSION
 
-setup(name='{}-{}'.format(USERNAME, 'gym-sw1'),
+setup(name='gym-seasun',
     version=VERSION,
-    description='Gym Env for SW1',
-    url='https://github.com/ksieee/gym_sw1',
+    description='Gym Env for Seasun Games',
+    url='https://github.com/ksieee/gym_seasun',
     author='Shane Li',
     author_email='ksieee@gmail.com',
     license='Apache V2 License',
-    packages=[package for package in find_packages() if package.startswith(USERNAME)],
-    package_data={ '{}_{}'.format(USERNAME, 'ksieee'): ['lua/*.lua', 'roms/*.nes' ] },
+    packages=[package for package in find_packages() if package.startswith('gym_')],
+    package_data={ 'gym_envs_seasun': ['lua/*.lua', 'roms/*.nes' ] },
     zip_safe=False,
     install_requires=[ 'gym>=0.8.0' ],
 )
