658cf0d5c702b674390af66539ca5604d4792014
Shane Lee <ksieee@gmail.com>
2017-05-05 19:41:07 +0800

    refine test env

--- gym_sw1/envs/test.py
+++ gym_sw1/envs/test.py
@@ -9,20 +9,19 @@ class Sw1TestEnv(Env):
         self.observation_space = np.array([1])
         self.action_space = np.array([1])
 
-    def _get_x_y(self):
-        x = np.linspace(-1, 1, 500)
-        y = 2 * x + 1
-        return x, y
+    def _get_y(self, x):
+        return x ** 2 + 0.5 * x + 1
 
     def _reset(self):
         # print('reset sw1 test env')
-        x, y = self._get_x_y()
+        x = np.linspace(-1, 1, 500)
+        y = self._get_y(x)
         plt.ion()
         plt.close('all')
         plt.figure(num='Test Figure')
         plt.plot(x, y)
         plt.draw()
-        plt.pause(0.1)
+        plt.pause(0.0001)
 
         self.step_count = 0
         self.current_state = np.random.uniform(low=-1,high=1,size=[1])
@@ -35,14 +34,14 @@ class Sw1TestEnv(Env):
     def _step(self, action):
         # print('step sw1 test env')
         curr_x = self.current_state[0]
-        curr_y = 2 * curr_x + 1
+        curr_y = self._get_y(curr_x)
         y = action[0]
 
         plt.plot(curr_x, y, 'rx')
         plt.draw()
-        plt.pause(0.1)
+        plt.pause(0.0001)
 
         self.step_count += 1
         self.current_state = np.random.uniform(low=-1,high=1,size=[1])
 
-        return self.current_state, -np.abs(y - curr_y), self.step_count >=5, None
+        return self.current_state, -np.abs(y - curr_y) * 10, self.step_count >=5, None
