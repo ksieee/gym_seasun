63047667d2ca3c4f1451de49a0b606cc6d51849b
周磊 <john.zhou2011@gmail.com>
2017-05-15 21:01:47 +0800

    Register normal attack

--- gym_sw1/__init__.py
+++ gym_sw1/__init__.py
@@ -3,5 +3,11 @@ from gym.envs.registration import register
 register(
     id='SW1-TEST-v0',
     entry_point='gym_sw1.envs:Sw1TestEnv',
-    max_episode_steps=10000,
+    max_episode_steps=10000
+)
+
+register(
+    id='SW1-NORMAL-ATTACK-v0',
+    entry_point='gym_sw1.envs:NormalAttackEnv',
+    max_episode_steps=500
 )
--- gym_sw1/envs/__init__.py
+++ gym_sw1/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_sw1.envs.test import Sw1TestEnv
+from gym_sw1.envs.normal_attack import NormalAttackEnv
